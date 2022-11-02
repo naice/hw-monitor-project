ee2fddacc9591f2e70ebc125b333b6d5ee173da9
Jens Marchewka <im.naice@googlemail.com>
2022-11-02 12:25:58 +0100

    fix interactive service issue.

--- INSTALL.ps1
+++ INSTALL.ps1
@@ -6,5 +6,6 @@ Stop-Service -Name hardware-monitor-server
 Remove-Service -Name hardware-monitor-server
 New-Service -Name hardware-monitor-server -BinaryPathName "$PSScriptRoot\bin\hw-monitor-server.exe --contentRoot $PSScriptRoot\bin" -Credential "$user" -Description "by Jens Marchewka" -DisplayName "Hardware Monitor Server" -StartupType Automatic
 Start-Service -Name hardware-monitor-server
-
+# mark as interactive otherwise the service cant get all data (such as temperatures i.e.)
+sc config hardware-monitor-server type=own type=interact
 pause
